9685df1950d1a15a5b0de72e1f0d2b81c5376aec
Mayanksaininh <mayanksaininh@gmail.com>
2026-06-25 17:34:25 +0530

    Setup cloudinary and Multer

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,18 +1,32 @@
 import { v2 as cloudinary } from 'cloudinary'; 
+import fs from "fs"
 
 const uploadCloudinary = async(filePath) => {
 
      // Configuration
      cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_NAME, 
-        api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_KEY 
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
     });
-    if(!filename) {
+
+    try {
+         if(!filePath) {
         return null 
     }
 
     // Upload an image
      const uploadResult = await cloudinary.uploader
-       .upload()
+       .upload(filePath)
+       fs.unlinkSync(filePath)
+       return uploadResult.secure_url
+    } 
+    
+    catch (error) {
+        fs.unlinkSync(filePath)
+        console.log(error);
+    }
+   
 }
+
+export default uploadCloudinary
