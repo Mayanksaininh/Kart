9fc465b4fd36ea7b6e4ab855dbfb1fba8e18af69
Mayanksaininh <mayanksaininh@gmail.com>
2026-06-28 23:45:32 +0530

    Fix UI for Add product component

--- admin/src/component/Add.jsx
+++ admin/src/component/Add.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import SideBar from "./SideBar";
+import { useState } from "react";
 
 const Add = () => {
+
+    const [image1 , setimage1] = useState()
+    const [image2 , setimage2] = useState()
+    const [image3 , setimage3] = useState()
+    const [image4 , setimage4] = useState()
+
   return (
     <div className="flex">
       
@@ -16,7 +23,7 @@ const Add = () => {
       ">
 
         {/* Heading */}
-        <h1 className="text-xl sm:text-2xl md:text-3xl font-semibold text-white text-center sm:text-left mb-6 border-b border-gray-700 pb-3">
+        <h1 className="text-xl sm:text-2xl md:text-3xl font-semibold text-white text-center sm:text-left mb-6 border-b border-gray-700 pb-3 mt-12">
           Add Product
         </h1>
 
@@ -37,6 +44,86 @@ const Add = () => {
                 placeholder="Enter product name"
               />
             </div>
+
+            <div>
+              <label className="block text-sm font-medium text-gray-100">
+                Description
+              </label>
+
+              <input
+                type="text"
+                className="mt-2 w-full rounded-md bg-white/5 px-3 py-2 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                placeholder="Enter Description"
+              />
+            </div>
+
+             <div>
+              <label className="block text-sm font-medium text-gray-100">
+                Price
+              </label>
+
+              <input
+                type="text"
+                className="mt-2 w-full rounded-md bg-white/5 px-3 py-2 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                placeholder="Enter Price"
+              />
+            </div>
+
+             <div>
+              <label className="block text-sm font-medium text-gray-100">
+                Category
+              </label>
+
+              <input
+                type="text"
+                className="mt-2 w-full rounded-md bg-white/5 px-3 py-2 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                placeholder="Enter Category"
+              />
+            </div>
+
+
+            <div className="flex gap-3 flex-wrap">
+             <label htmlFor="image1" className="block text-sm font-medium text-gray-100">
+               <img
+                    src={image1 || "https://png.pngtree.com/png-vector/20250415/ourmid/pngtree-icon-upload-clipart-blue-button-vector-file-png-image_15952470.png"}
+                    className="w-20 h-20 object-cover rounded-lg border cursor-pointer"
+                />
+                <input 
+                type="file"
+                id="image1" hidden onChange={(e) => setimage1(e.target.files[0])}/>
+            </label>
+
+            <label htmlFor="image2" className="block text-sm font-medium text-gray-100">
+               <img
+                    src={image2 || "https://png.pngtree.com/png-vector/20250415/ourmid/pngtree-icon-upload-clipart-blue-button-vector-file-png-image_15952470.png"}
+                    className="w-20 h-20 object-cover rounded-lg border cursor-pointer"
+                />
+                <input 
+                type="file"
+                id="image2" hidden onChange={(e) => setimage2(e.target.files[0])}/>
+            </label>
+
+            <label htmlFor="image3" className="block text-sm font-medium text-gray-100">
+               <img
+                    src={image3 || "https://png.pngtree.com/png-vector/20250415/ourmid/pngtree-icon-upload-clipart-blue-button-vector-file-png-image_15952470.png"}
+                    className="w-20 h-20 object-cover rounded-lg border cursor-pointer"
+                /> 
+                <input
+                type="file"
+                id="image3" hidden onChange={(e) => setimage3(e.target.files[0])}/>
+            </label>
+
+            <label htmlFor="image4" className="block text-sm font-medium text-gray-100">
+                <img
+                        src={image4 || "https://png.pngtree.com/png-vector/20250415/ourmid/pngtree-icon-upload-clipart-blue-button-vector-file-png-image_15952470.png"}
+                        className="w-20 h-20 object-cover rounded-lg border cursor-pointer"
+                />
+                <input 
+                type="file"
+                id="image4" hidden onChange={(e) => setimage4(e.target.files[0])}/>
+            </label>
+            </div>
+
 
             {/* Button */}
             <button
--- admin/src/component/Header.jsx
+++ admin/src/component/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import LogOut from "./Logout";
 
-const Header = () =>{
+const Header = () => {
     return (
-        <div className="w-full bg-gradient-to-b from-black/100 to-black/20 h-16 flex items-center justify-between px-0">
+        <div className="fixed top-0 left-0 w-full bg-gradient-to-b from-black/100 to-black/20 h-16 flex items-center justify-between px-0 z-50">
             <div className="flex items-center">
                     <img className="h-10 md:h-12 w-auto" 
                     src="https://static.vecteezy.com/system/resources/thumbnails/026/956/862/small/shopping-cart-basket-png.png" 
