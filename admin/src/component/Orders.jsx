4cfcd8e504b2021160f9ddc4414b987ecdc0d373
Mayanksaininh <mayanksaininh@gmail.com>
2026-07-18 18:52:53 +0530

    Make sidebar responsive in order component (admin)

--- admin/src/component/Orders.jsx
+++ admin/src/component/Orders.jsx
@@ -6,6 +6,11 @@ const Orders = () =>{
     return (
         <div>
             <SideBar/>
+            <div className=" w-full min-h-screen py-10
+            px-4 sm:px-6 md:px-10
+            ml-[120px] sm:ml-[40%] md:ml-[25%] lg:ml-[18%]">
+
+            </div>
             Orders
         </div>
     )
