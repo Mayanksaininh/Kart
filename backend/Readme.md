43e853dc7439960729e4bd20f238ea56ea43f531
Mayanksaininh <mayanksaininh@gmail.com>
2026-08-01 18:35:46 +0530

    Create Model for Orders ,

--- backend/Readme.md
+++ backend/Readme.md
@@ -22,4 +22,5 @@
 -Create controller for Remove product ,
 -Create route for remove and List Product ,
 -Create controller for addtocart , updatecart , getusercart ,
-Create Routes for addtocart , updatecart , getusercart ,
+-Create Routes for addtocart , updatecart , getusercart ,
+-Create Model for Orders ,
---
+++ backend/models/orderModel.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose" 
+
+const orderSchema = new mongoose.Schema({
+   userID : {
+        type : String,
+        required : true
+    },
+
+    items : {
+        type : Array,
+        required : true,
+    },
+
+    amount : {
+        type : Number ,
+        required : true,
+    },
+
+    address : {
+        type : Object,
+        required : true
+    },
+
+    status : {
+        type : String , 
+        required : true,
+        default : 'Order Placed'
+    },
+
+    payment : {
+        type : Boolean,
+        required : true,
+        default : false
+    },
+
+    date : {
+        type : Number,
+        required : true,
+    }
+} ,{timestamps : true}) 
+
+
+const Order = mongoose.model("Order" , orderSchema)
+
+export default  orderSchema
