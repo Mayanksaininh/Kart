41a958bbe3df171d50bd5bf88e534b522ce1cb00
Mayanksaininh <mayanksaininh@gmail.com>
2026-08-05 20:58:43 +0530

    Making Form for delivery information

--- frontend/src/components/placeorder.jsx
+++ frontend/src/components/placeorder.jsx
@@ -10,7 +10,35 @@ const PlaceOrder = () => {
 
                 <div className="w-[100%] h-[70px] flex items-center justify-between px-[10px]">
                     <input type="text" placeholder="First name" className="w-[48%] h-[50px] rounded-md bg-slate-700 placeholder:text[white] text-[18px] px-[20px] shadow-sm shadow-[#343434]" required></input>
+
+                    <input type="text" placeholder="Last name" className="w-[48%] h-[50px] rounded-md bg-slate-700 placeholder:text[white] text-[18px] px-[20px] shadow-sm shadow-[#343434]" required></input>
+                
                 </div>
+
+                <div className="w-[100%] h-[70px] flex items-center justify-between px-[10px]">
+                    <input type="text" placeholder="Email" className="w-[48%] h-[50px] rounded-md bg-slate-700 placeholder:text[white] text-[18px] px-[20px] shadow-sm shadow-[#343434]" required></input>
+                </div>
+
+                 <div className="w-[100%] h-[70px] flex items-center justify-between px-[10px]">
+                    <input type="text" placeholder="Street" className="w-[48%] h-[50px] rounded-md bg-slate-700 placeholder:text[white] text-[18px] px-[20px] shadow-sm shadow-[#343434]" required></input>
+
+                    <input type="text" placeholder="Land-Mark" className="w-[48%] h-[50px] rounded-md bg-slate-700 placeholder:text[white] text-[18px] px-[20px] shadow-sm shadow-[#343434]" required></input>
+                </div>
+
+                <div className="w-[100%] h-[70px] flex items-center justify-between px-[10px]">
+                    <input type="text" placeholder="City" className="w-[48%] h-[50px] rounded-md bg-slate-700 placeholder:text[white] text-[18px] px-[20px] shadow-sm shadow-[#343434]" required></input>
+
+                    <input type="text" placeholder="Pincode" className="w-[48%] h-[50px] rounded-md bg-slate-700 placeholder:text[white] text-[18px] px-[20px] shadow-sm shadow-[#343434]" required></input>
+                </div>
+
+                <div className="w-[100%] h-[70px] flex items-center justify-between px-[10px]">
+                    <input type="text" placeholder="State" className="w-[48%] h-[50px] rounded-md bg-slate-700 placeholder:text[white] text-[18px] px-[20px] shadow-sm shadow-[#343434]" required></input>
+                </div>
+
+                 <div className="w-[100%] h-[70px] flex items-center justify-between px-[10px]">
+                    <input type="text" placeholder="Contact Number" className="w-[48%] h-[50px] rounded-md bg-slate-700 placeholder:text[white] text-[18px] px-[20px] shadow-sm shadow-[#343434]" required></input>
+                </div>
+
             </form>
             
         </div>
