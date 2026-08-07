bcf460c1e72461ad91398272f537c3b92e791a34
Mayanksaininh <mayanksaininh@gmail.com>
2026-08-08 02:36:51 +0530

    Add CartTotal at placeOrder component

--- frontend/src/components/placeorder.jsx
+++ frontend/src/components/placeorder.jsx
@@ -1,8 +1,11 @@
-import React from "react";
-import CartTotal from "./cartTotal";
+import React , { useContext } from "react";
+import { ShopDataContext } from "../context/ShopContext";
 
 const PlaceOrder = () => {
+  
+    const {currency , delivery_fee, getcartAmount} = useContext(ShopDataContext)
     return (
+      <div>
         <div className="lg:w-[50%] w-full h-full flex items-center justify-center lg:mt-0 mt-16 px-4">
   <form className="w-full lg:w-[70%] flex flex-col gap-4">
     
@@ -69,6 +72,33 @@ const PlaceOrder = () => {
   </button>
 </div>
   </form>
+</div>
+      <div>
+        <div className="w-full sm:w-[90%] md:w-[70%] lg:w-[40%] xl:w-[35%] bg-slate-700 rounded-xl p-4 sm:p-6 flex flex-col gap-3 sm:gap-4 text-white mx-auto lg:mx-0">
+              <h2 className="text-lg sm:text-xl font-semibold border-b border-gray-500 pb-3">
+        Cart Total
+      </h2>
+
+      <div className="flex flex-col gap-2 sm:gap-3 text-xs sm:text-sm">
+        <div className="flex items-center justify-between">
+          <span className="text-gray-300">Subtotal</span>
+          <span className="font-medium">{currency} {getcartAmount()}.00</span>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <span className="text-gray-300">Delivery Fee</span>
+          <span className="font-medium">
+            {currency} {delivery_fee}.00
+          </span>
+        </div>
+
+        <div className="flex items-center justify-between border-t border-gray-500 pt-3 text-sm sm:text-base font-semibold">
+          <span>Total</span>
+          <span className="text-[#9ff9f9]">{currency} {getcartAmount() === 0 ? 0 : getcartAmount() + delivery_fee}.00</span>
+        </div>
+      </div>
+        </div>
+      </div>
 
 </div>
     )
